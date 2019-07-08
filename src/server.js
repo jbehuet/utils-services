67a4f49e58f6e6f985c0b9e7e055dd7aa5923a60
jbehuet <jbehuet@gmail.com>
2019-07-08 19:24:36 +0200

    Add missing method CORS

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', "*");
         res.setHeader('Access-Control-Request-Method', "*");
         res.setHeader('Access-Control-Allow-Headers', "*");
-        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
     }
     next();
 })
